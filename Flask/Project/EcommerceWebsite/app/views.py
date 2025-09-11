d7a562012c58d3c48914c6a10ad43db064ca38eb
Sayeeram05 <sr6172@srmist.edu.in>
2025-09-11 20:14:05 +0530

    App : Aboutus updated

--- Flask/Project/EcommerceWebsite/app/views.py
+++ Flask/Project/EcommerceWebsite/app/views.py
@@ -5,3 +5,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template('home.html')
+
+@views.route('/aboutus')
+def about_us():
+    return render_template('aboutus.html')
