8eed207eceb3c5bff2a6fff18bf5b500e1742342
Sayeeram05 <sr6172@srmist.edu.in>
2025-09-17 12:30:10 +0530

    Remove Django/Authentication directory

--- Django/Authentication/Auth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import login_view, signup_view
-from Frontend.views import home
-
-urlpatterns = [
-    path('login/', login_view, name='login'), 
-    path('signup/', signup_view, name='signup'),
-    path('', home, name='home'),  
-]
