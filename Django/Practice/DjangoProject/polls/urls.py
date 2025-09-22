7a3af5c9dfec3dba1f4df5becac6068c03ad975f
Sayeeram05 <sr6172@srmist.edu.in>
2025-09-22 16:00:16 +0530

    Django App : Part 3

--- Django/Practice/DjangoProject/polls/urls.py
+++ Django/Practice/DjangoProject/polls/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
+
 urlpatterns = [
-    path('', view=views.index, name='index')
+    path('', view=views.index, name='index'),
+    path('<int:question_id>/',views.detail , name="details"),
+    path('<int:question_id>/result/',views.results , name="result"),
+    path('<int:question_id>/vote/',views.vote , name="vote")
+    
+    
 ]
